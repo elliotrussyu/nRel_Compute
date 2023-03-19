a1aed8c6a4a8fb72dd3676f64131e4c4c7b3a874
elliotrussyu <64067074+elliotrussyu@users.noreply.github.com>
2023-03-19 11:50:30 +0100

    Update 19032023
    Minor fix for previous code.
    Added function for computing node reliability coefficients from connected set coefficients.

--- NodeRel.py
+++ NodeRel.py
@@ -3,46 +3,68 @@ import itertools as it
 import numpy as np
 import igraph as ig
 import time 
+import math
 
 
-def combi(N,num):
+def combi( N:int, num:int)->it.combinations:
     """
         Find all comibinations of 'num' elements for int in [0,N-1].
         
         Input: 
-            N       total number of elements (numbering starts from zero!)
-            num     number of elements in one combination
+            N     total number of elements (numbering starts from zero!)
+            num   number of elements in one combination
 
         Output:
-            C       the list with all combinations
+            C     the list with all combinations
     """
     return it.combinations(tuple(range(N)),num)
 
 
-def combi_all(N):
+def combi_all(N:int) -> tuple:
     """
     Find all comibinations for int in [0,N-1].
     
     Input: 
-        N       total number of elements (numbering starts from zero!)
+        N    total number of elements (numbering starts from zero!)
 
     Output:
-        AC       the list with all combinations, arranged from the empty set to N-element sets
+        AC   the list with all combinations, arranged from the empty set to N-element sets
     """
     return tuple(map(combi,it.repeat(N),range(N+1)))
 
 
-def examine_connected_sets(G,N_minus_f):
+def examine_connected_sets(G: ig.Graph,f: int) -> int:
+    """
+    This function enumerates the connected sets of cardinality k = N-f. f represents the cardinality of the failure node set.
+    Input: 
+        G    a igraph.Graph object
+        f    number of nodes to be removed
+
+    Output:
+        c_n_minus_f   the cardinality of the k-ordered connected sets.
+    """
+    
     c_N_minus_f = 0
-    for failed_vertice_set in combi(G.vcount(),N_minus_f):
+    for failed_vertice_set in combi(G.vcount(),f):
         GG = G.copy()
         GG.delete_vertices(failed_vertice_set)
         if GG.is_connected() == True:
             c_N_minus_f += 1
-    print(c_N_minus_f)
     return c_N_minus_f
 
-def to_ig(H):
+def to_ig(H) -> ig.Graph:
+    """
+    This function converts the input graph H to the correponding igraph Graph object.
+    Supported format of H is described below.
+    Input:
+        H       H can be a list of tuples that describes all edges in a graph, 
+                a numpy matrix that represents the adjacency matrix,
+                an igraph graph object,
+                or a networkx graph object.
+    Output:
+        G       An igraph graph object that correspond to the input graph H.
+    """
+    
     if type(H) is nx.classes.graph.Graph:
         G0 = H.copy()
         N = G0.order()
@@ -60,22 +82,24 @@ def to_ig(H):
     return G
 
 
-def connected_set_poly(H):
+def connected_set_poly(H) -> np.ndarray:
     """
     This function generates the connected set node polynomial of a graph
     Input:
         H       H can be a list of tuples that describes all edges in a graph, 
-                or a networkx graph.
+                a numpy matrix that represents the adjacency matrix,
+                an igraph graph object,
+                or a networkx graph object.
     Output:
-        s       The node connected set polynomial coefficients, 
-                begins with the leading coefficient(highest order) and ends with the constant coefficient(zero order)
+        c_k       The node connected set polynomial coefficients, 
+                begins with the leading coefficient(highest degree) and ends with the constant coefficient(zero degree)
     """
     #Processing the input graph, convert input H to an igraph graph
     G = to_ig(H) 
     N = G.vcount()
     
     c_k = np.zeros(N+1) 
-    # Initiallize the array of k-ordered connected set coefficient c_k. 
+    # Initiallize the array of k-cardinality connected set coefficient c_k. 
     # Where the numpy array c_k start with the leading coefficient when k = N. 
     # => c_k[0] is c_N, c_k[N+1] is c_(N-1), ...,
     # c_k[N-1] is c_1, and c_k[N] is c_0 which is always 0. 
@@ -84,7 +108,30 @@ def connected_set_poly(H):
         if N>1:
             c_k[N-2] = G.ecount()
             kappa_det = G.cohesion()
-            # Kappa is the vertex connectivity of a graph, then for the first kappa highest order coefficients, the value can be directly calculated via the binomial coefficients. 
+            # Kappa is the vertex connectivity of a graph, then for the first kappa-number terms of highest degree, the coefficients can be efficiently calculated using the binomial coefficients. 
             c_k[0:kappa_det] = np.array(list(map(math.comb,it.repeat(N),range(N-kappa_det+1,N+1))))[::-1]
+            # For the rest of the coefficients, enumerate all possible sets and count the connected sets. 
             c_k[kappa_det:N-2] = np.array(list(map(examine_connected_sets,[G.copy() for _ in range(kappa_det,N-2)],range(kappa_det,N-2))))
     return c_k.astype(int)
+
+
+def nRelpoly(H):
+    """
+    Calculates the coefficients of node reliability polynomial for the given graph H.
+    Input:
+        H    H can be a list of tuples that describes all edges in a graph, 
+             or a networkx graph.
+    Output:
+        coeff    The array of coefficients of the node reliability polynomial for the graph.
+    """
+    G = to_ig(H)
+    N = G.vcount()
+    # Get the connected-set polynomial coefficients
+    c_k = connected_set_poly(G)[::-1]
+    coeff = np.zeros(N+1)
+    # Find the coefficient from the definition of node reliability polynomial. 
+    # c_k*(p^k)*(1-p)^(N-k)
+    for k in range(N+1):
+        coeff[k:] += np.array([c_k[k]*((-1)**i)*math.comb(N-k,i) for i in range(N-k+1)])
+    
+    return coeff[::-1]
